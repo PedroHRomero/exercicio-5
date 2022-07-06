7541c844ec390edb3672c98ab9497110923c5e0c
pedro <pedro@DESKTOP-R8CBM62>
2022-07-06 16:57:01 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i+=2)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.WriteLine("Esses são os numeros pares entre 1 e 100");
+            
+            Console.ReadKey();
+        }
+    }
+}
